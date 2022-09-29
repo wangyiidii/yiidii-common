45277b9958d6ec1727deb97b2e66302f238bd1b2
wangyidi <wangyidi@huice.com>
2022-09-29 11:47:28 +0800

    支持String返回

--- yiidii-base/src/main/java/cn/yiidii/base/util/ProcessUtil.java
+++ yiidii-base/src/main/java/cn/yiidii/base/util/ProcessUtil.java
@@ -4,6 +4,7 @@ import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.exceptions.ExceptionUtil;
 import cn.hutool.core.text.StrPool;
 import cn.hutool.core.util.RuntimeUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.yiidii.base.domain.dto.ProcessResultDTO;
 
 import java.util.ArrayList;
@@ -15,6 +16,48 @@ import java.util.List;
  * @author ed w
  */
 public class ProcessUtil {
+
+    /**
+     * 执行脚本，结果输出为String
+     *
+     * @param cmd 命令
+     * @return {@link ProcessResultDTO}
+     */
+    public static ProcessResultDTO<String> execForStr(String cmd) {
+        Process process = null;
+        try {
+            process = RuntimeUtil.exec(cmd);
+            int exitCode = process.waitFor();
+            String result = RuntimeUtil.getResult(process);
+            return new ProcessResultDTO<>(exitCode, StrUtil.trim(result), StrUtil.trim(result), "");
+        } catch (Throwable e) {
+            return ProcessResultDTO.error(-1, null, ExceptionUtil.getSimpleMessage(e));
+        } finally {
+            RuntimeUtil.destroy(process);
+        }
+    }
+
+
+    /**
+     * 执行脚本，结果输出为list
+     *
+     * @param cmd 命令
+     * @return {@link ProcessResultDTO}
+     */
+    public static ProcessResultDTO<List<String>> execForLines(String cmd) {
+        Process process = null;
+        try {
+            process = RuntimeUtil.exec(cmd);
+            int exitCode = process.waitFor();
+            List<String> result = RuntimeUtil.getResultLines(process);
+            return new ProcessResultDTO<>(exitCode, result, CollUtil.join(result, StrPool.CRLF), "");
+        } catch (Throwable e) {
+            return ProcessResultDTO.error(-1, new ArrayList<>(), ExceptionUtil.getSimpleMessage(e));
+        } finally {
+            RuntimeUtil.destroy(process);
+        }
+    }
+
 
     /**
      * 执行脚本，结果转换成对应Class
@@ -49,26 +92,6 @@ public class ProcessUtil {
             int exitCode = process.waitFor();
             String result = RuntimeUtil.getResult(process);
             return new ProcessResultDTO<>(exitCode, JsonUtils.parseArray(result, clazz), result, "");
-        } catch (Throwable e) {
-            return ProcessResultDTO.error(-1, new ArrayList<>(), ExceptionUtil.getSimpleMessage(e));
-        } finally {
-            RuntimeUtil.destroy(process);
-        }
-    }
-
-    /**
-     * 执行脚本，结果输出为list
-     *
-     * @param cmd 命令
-     * @return {@link ProcessResultDTO}
-     */
-    public static ProcessResultDTO<List<String>> execForLines(String cmd) {
-        Process process = null;
-        try {
-            process = RuntimeUtil.exec(cmd);
-            int exitCode = process.waitFor();
-            List<String> result = RuntimeUtil.getResultLines(process);
-            return new ProcessResultDTO<>(exitCode, result, CollUtil.join(result, StrPool.CRLF), "");
         } catch (Throwable e) {
             return ProcessResultDTO.error(-1, new ArrayList<>(), ExceptionUtil.getSimpleMessage(e));
         } finally {
